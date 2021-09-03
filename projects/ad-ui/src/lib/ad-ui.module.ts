23e64253ef038dd60c7c38f2421ec0e58fdb9b47
qichangjun <1061427304@qq.com>
2021-09-03 15:30:44 +0800

    perf:更新版本，删除无用代码

--- projects/ad-ui/src/lib/ad-ui.module.ts
+++ projects/ad-ui/src/lib/ad-ui.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { AdUiComponent } from './ad-ui.component';
 import { AdTreeModule } from './ad-tree/ad-tree.module';
 import { AdScrollBoxModule } from './ad-scroll-box/ad-scroll-box.module';
-import { scrollBoxBtn } from './ad-scroll-box/ad-scroll-box-btn.directive';
-import { AdScrollBoxComponent } from './ad-scroll-box/ad-scroll-box.component';
+
 import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [AdUiComponent],
